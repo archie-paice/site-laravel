c5a11d3b12aebb97dcefbdcaa79f259a262eaa82
chrisjm66 <chrisjm66@gmail.com>
2025-10-20 23:36:57 -0400

    Navbar stuff

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,9 @@
-<div>
-    <h1>ZJX ARTCC</h1>
-    <h1>{{  auth()->user()->first_name }} {{  auth()->user()->last_name }} - {{  auth()->user()->id }}</h1>
+<div class="navbar bg-primary text-primary-content">
+    <div class="flex-1">
+        <h1>ZJX ARTCC</h1>
+    </div>
+
+    <div>
+        <h1>{{  auth()->user()->first_name }} {{  auth()->user()->last_name }} - {{  auth()->user()->id }}</h1>
+    </div>
 </div>
