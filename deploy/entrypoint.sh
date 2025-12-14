e396ae8a707ad63a2f481bea9d1a656cef944a76
chrisjm66 <chrisjm66@gmail.com>
2025-12-14 13:19:05 -0500

    Update entrypoint to add app key properly

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -4,7 +4,7 @@ php artisan config:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-php artisan key:generate
+php artisan key:generate --show >> ../.env.prod
 php artisan storage:link
 
 
