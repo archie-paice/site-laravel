a0ed3b3866459e1e5286f2130673ad5108237661
chrisjm66 <chrisjm66@gmail.com>
2025-12-14 21:39:10 -0500

    Fix oauth

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
     'vatsim' => [
         'client_id' => env('VATSIM_CLIENT_ID'),
         'client_secret' => env('VATSIM_CLIENT_SECRET'),
-        'redirect' => env('APP_URL').'/auth/callback',
+        'redirect' => '/auth/callback',
     ],
 
 ];
