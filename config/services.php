fc39ec3985a4a8acd1dfbfe30a621f88f9e71cce
chrisjm66 <chrisjm66@gmail.com>
2025-12-14 21:23:40 -0500

    plsss

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
     'vatsim' => [
         'client_id' => env('VATSIM_CLIENT_ID'),
         'client_secret' => env('VATSIM_CLIENT_SECRET'),
-        'redirect' => '/auth/callback',
+        'redirect' => env('APP_URL').'/auth/callback',
     ],
 
 ];
