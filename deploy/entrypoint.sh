5fac56248847bc8783c2e0146d54c4869a2f9070
chrisjm66 <chrisjm66@gmail.com>
2025-12-14 22:44:31 -0500

    Fix mounting

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+set -e
+
 echo "Caching configuration..."
 php artisan optimize:clear
 php artisan config:cache
@@ -5,5 +8,5 @@ php artisan route:cache
 php artisan view:cache
 php artisan storage:link
 
-echo "Starting PHP-FPM..."
-exec php-fpm
+echo "Starting Laravel HTTP server on 0.0.0.0:8080"
+exec php artisan serve --host=0.0.0.0 --port=8080
