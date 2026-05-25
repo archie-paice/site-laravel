33da5d6e7ad563cae19f47007a6bac4be791c714
Archie <archiepaicefi@gmail.com>
2026-05-25 20:37:54 +0100

    Fix staff card layout: equal-height cards with Reports to pinned to bottom
    Make card-component merge incoming classes so staff cards can pass h-full,
    giving all cards in a grid row equal height. Move the "Reports to:" line to a
    bottom-anchored footer (mt-auto) so it lines up across cards regardless of
    description length or assistant count.

--- resources/views/components/card-component.blade.php
+++ resources/views/components/card-component.blade.php
@@ -1,4 +1,4 @@
-<div class="card border-1 border-base-300 p-5">
+<div {{ $attributes->merge(['class' => 'card border-1 border-base-300 p-5']) }}>
     @unless(is_null($title ?? null))
         <h1 class="card-title text-lg font-bold leading-snug">{{ $title }}</h1>
     @endunless
--- resources/views/components/staff-card.blade.php
+++ resources/views/components/staff-card.blade.php
@@ -1,4 +1,4 @@
-<x-card-component :title="$position">
+<x-card-component :title="$position" class="h-full">
     <div class="flex flex-col h-full content-stretch">
         @unless(is_null($staff))
             <a href="{{route('users.show', ['user' => $staff->user->id])}}" class="text-2xl mb-1">{{ $staff->user->first_name.' '.$staff->user->last_name }} ({{$staff->user->rating->mapToString()}})</a>
@@ -8,8 +8,9 @@
         @endunless
 
         <p class="text-lg">{{$description}}</p>
-        <p class="text-lg mt-5"><strong>Reports to:</strong> {{$reportsTo}}</p>
 
         {{$slot}}
+
+        <p class="text-lg mt-auto pt-5"><strong>Reports to:</strong> {{$reportsTo}}</p>
     </div>
 </x-card-component>
