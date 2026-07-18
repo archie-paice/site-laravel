5ed5d46e5f3480070a513c0d73deac4d0a629d11
Chris Mangan <chrisjm66@gmail.com>
2026-07-17 21:11:37 -0400

    Oops

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -11,19 +11,11 @@ class PermissionSeeder extends Seeder
     private array $permissions = [
         'core' => ['edit own profile'],
         'staff' => ['view dashboard'],
-<<<<<<< HEAD
         'admin' => ['manage users', 'assign roles', 'manage roles', 'view audit logs', 'manage visiting controllers', 'documents:write' ],
         'events' => ['manage events', 'assign event positions', 'publish events'],
         'facilities' => ['manage statistics prefixes', 'manage certification facilities', 'statistics:write', 'documents:write'],
         'training' => ['create training tickets', 'edit training tickets', 'claim students', 'issue solo certs', 'training-tickets:read', 'training-assignments:read', 'solo-certs:read' ],
         'instructor' => ['revoke solo certs', 'manage training tickets', 'manage students']
-=======
-        'admin' => ['manage users', 'assign roles', 'manage roles', 'view audit logs', 'manage visiting controllers'],
-        'events' => ['manage events', 'assign event positions', 'publish events'],
-        'facilities' => ['manage statistics prefixes', 'manage certification facilities'],
-        'training' => ['create training tickets', 'edit training tickets', 'claim students', 'issue solo certs', 'training-tickets:write'],
-        'instructor' => ['revoke solo certs', 'manage training tickets', 'manage students', 'training-tickets:write'],
->>>>>>> origin/main
     ];
 
     /**
